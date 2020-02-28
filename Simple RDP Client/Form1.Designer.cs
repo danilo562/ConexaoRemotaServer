bee589764be9631029aa49ebc0cae5110d549a93
danilo de souza novaes <danilo.novaes@hotmail.com.br>
2020-02-27 21:11:35 -0300

    botao

---
+++ Simple RDP Client/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace Simple_RDP_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textConnectionString = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.axRDPViewer = new AxRDPCOMAPILib.AxRDPViewer();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lbNome = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnFinalizar = new System.Windows.Forms.Button();
+            this.lbId = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.axRDPViewer)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textConnectionString
+            // 
+            this.textConnectionString.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.textConnectionString.Location = new System.Drawing.Point(29, 375);
+            this.textConnectionString.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textConnectionString.Name = "textConnectionString";
+            this.textConnectionString.Size = new System.Drawing.Size(77, 22);
+            this.textConnectionString.TabIndex = 1;
+            this.textConnectionString.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button1.Location = new System.Drawing.Point(277, 373);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 28);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "CONECTAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // axRDPViewer
+            // 
+            this.axRDPViewer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.axRDPViewer.Enabled = true;
+            this.axRDPViewer.Location = new System.Drawing.Point(6, 9);
+            this.axRDPViewer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.axRDPViewer.Name = "axRDPViewer";
+            this.axRDPViewer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axRDPViewer.OcxState")));
+            this.axRDPViewer.Size = new System.Drawing.Size(632, 249);
+            this.axRDPViewer.TabIndex = 5;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(16, 15);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(869, 353);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.axRDPViewer);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Size = new System.Drawing.Size(861, 324);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "VIEW";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.btnAtualizar);
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage2.Size = new System.Drawing.Size(861, 324);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "LISTA";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.Location = new System.Drawing.Point(332, 7);
+            this.btnAtualizar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(144, 28);
+            this.btnAtualizar.TabIndex = 1;
+            this.btnAtualizar.Text = "ATUALIZAR";
+            this.btnAtualizar.UseVisualStyleBackColor = true;
+            this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column2,
+            this.Column1,
+            this.Column3,
+            this.Column6,
+            this.Column4,
+            this.Column5,
+            this.Column7});
+            this.dataGridView1.Location = new System.Drawing.Point(8, 43);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(843, 271);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "string";
+            this.Column2.HeaderText = "string";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Visible = false;
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "maquina";
+            this.Column1.HeaderText = "MAQUINA";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 130;
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "dtcad";
+            this.Column3.HeaderText = "DATA SOLICITACAO";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column6
+            // 
+            this.Column6.DataPropertyName = "processador";
+            this.Column6.HeaderText = "PROCESSADOR";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Width = 300;
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "memoria";
+            this.Column4.HeaderText = "MEMORIA";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.DataPropertyName = "windows";
+            this.Column5.HeaderText = "WINDOWS";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Width = 300;
+            // 
+            // Column7
+            // 
+            this.Column7.DataPropertyName = "idM";
+            this.Column7.HeaderText = "ID";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            // 
+            // lbNome
+            // 
+            this.lbNome.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.lbNome.AutoSize = true;
+            this.lbNome.Location = new System.Drawing.Point(116, 384);
+            this.lbNome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbNome.Name = "lbNome";
+            this.lbNome.Size = new System.Drawing.Size(101, 17);
+            this.lbNome.TabIndex = 7;
+            this.lbNome.Text = "SEL MAQUINA";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button2.Location = new System.Drawing.Point(466, 372);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 28);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "DESCONECTAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnFinalizar
+            // 
+            this.btnFinalizar.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnFinalizar.Location = new System.Drawing.Point(787, 373);
+            this.btnFinalizar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnFinalizar.Name = "btnFinalizar";
+            this.btnFinalizar.Size = new System.Drawing.Size(99, 28);
+            this.btnFinalizar.TabIndex = 9;
+            this.btnFinalizar.Text = "FINALIZAR";
+            this.btnFinalizar.UseVisualStyleBackColor = true;
+            this.btnFinalizar.Click += new System.EventHandler(this.btnFinalizar_Click);
+            // 
+            // lbId
+            // 
+            this.lbId.AutoSize = true;
+            this.lbId.Location = new System.Drawing.Point(623, 384);
+            this.lbId.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbId.Name = "lbId";
+            this.lbId.Size = new System.Drawing.Size(19, 17);
+            this.lbId.TabIndex = 10;
+            this.lbId.Text = "id";
+            this.lbId.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 407);
+            this.Controls.Add(this.lbId);
+            this.Controls.Add(this.btnFinalizar);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lbNome);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textConnectionString);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "ATENDER";
+            ((System.ComponentModel.ISupportInitialize)(this.axRDPViewer)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textConnectionString;
+        private System.Windows.Forms.Button button1;
+        private AxRDPCOMAPILib.AxRDPViewer axRDPViewer;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lbNome;
+        private System.Windows.Forms.Button btnAtualizar;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnFinalizar;
+        private System.Windows.Forms.Label lbId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+    }
+}
+
---
+++ Simple RDP Client/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RDPCOMAPILib;
+using AxRDPCOMAPILib;
+using System.Data.SqlClient;
+using System.Data.Odbc;
+
+namespace Simple_RDP_Client
+{
+    public partial class Form1 : Form
+    {
+        OdbcConnection conexao = new OdbcConnection(System.Configuration.ConfigurationManager.ConnectionStrings["programas1"].ConnectionString.ToString());
+    
+        public Form1()
+        {
+            InitializeComponent();
+            carregaUSUARIO(conexao);
+        }
+
+        public static void Connect(string invitation, AxRDPViewer display, string userName, string password)
+        {
+            display.Connect(invitation, userName, password);
+        }
+
+        public static void disconnect(AxRDPViewer display)
+        {
+            display.Disconnect();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Connect(textConnectionString.Text, this.axRDPViewer, "", "");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Unable to connect to the Server");
+            }
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textConnectionString.Text = (dataGridView1.CurrentRow.Cells[0].Value).ToString();
+            lbNome.Text = (dataGridView1.CurrentRow.Cells[1].Value).ToString();
+            lbId.Text = (dataGridView1.CurrentRow.Cells[6].Value).ToString();
+
+
+            tabControl1.SelectedIndex = 0;
+        }
+
+        public void carregaUSUARIO(OdbcConnection conexao)
+        {
+            if (conexao.State == ConnectionState.Closed) //Validar a conexão
+            {
+                conexao.Open();
+            }
+
+            //este metodo esta carregando os nome dos modulos
+            try
+            {
+
+                OdbcDataAdapter ADAP = new OdbcDataAdapter("SELECT string,maquina,dtcad,processador,memoria,windows,idM FROM conexaoRemota where delet =''", conexao);
+                DataSet DS = new DataSet();
+
+                
+                ADAP.Fill(DS, "fornecedor");
+
+                
+                dataGridView1.DataSource = DS.Tables["fornecedor"];
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            conexao.Close();
+
+        }
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            carregaUSUARIO(conexao);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            disconnect(this.axRDPViewer);
+        }
+
+        private void btnFinalizar_Click(object sender, EventArgs e)
+        {
+            if (lbId.Text == "id")
+            {
+
+            }
+            else {
+                finalizarConexao(conexao, lbId.Text);
+                lbNome.Text = "";
+            }
+        }
+
+        public void finalizarConexao(OdbcConnection conexao, string id)
+        {
+            if (conexao.State == ConnectionState.Closed) //Validar a conexão
+            {
+                conexao.Open();
+            }
+
+            try
+            {
+                string sql = "update conexaoRemota set delet='*' where idM='" + id + "'";
+
+                OdbcCommand cmd1 = new OdbcCommand(sql, conexao);
+
+                cmd1.ExecuteNonQuery();
+                carregaUSUARIO(conexao);
+            }
+            catch { conexao.Close(); }
+
+            conexao.Close();
+        }
+
+
+
+    }
+}
